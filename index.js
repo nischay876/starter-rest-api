15934b852cab524f8eed9dec27c930626eb58562
Nischay Sharma <nischay876@gmail.com>
2023-05-24 14:52:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const db = require('@cyclic.sh/dynamodb')
-const authenticate = require('./auth');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -19,6 +18,23 @@ app.use(express.urlencoded({ extended: true }))
 // }
 // app.use(express.static('public', options))
 // #############################################################################
+
+const authenticate = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    // Check if the provided token matches a specific value (e.g., '5555-5555-5555-5555')
+    if (token !== '5555-5555-5555-5555') {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    // If the token is valid, proceed to the next middleware
+    next();
+  } catch (error) {
+    console.error('Authentication error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 
 // Create or Update an item
 app.post('/:col/:key', authenticate, async (req, res) => {
